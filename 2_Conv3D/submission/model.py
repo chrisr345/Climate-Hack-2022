eda84e4e60b7cd597347309bf40d1996122fc789
ttdelgadott <christophercross10@gmail.com>
2022-03-14 23:54:02 -0700

    modifications

--- 2_Conv3D/submission/model.py
+++ 2_Conv3D/submission/model.py
@@ -8,10 +8,11 @@ class Model(nn.Module):
 
         self.down_pool = nn.MaxPool3d(kernel_size=(1, 2, 2))
         self.layer1 = nn.Conv3d(1, 4, stride=(1, 2, 2), kernel_size=5)
-        self.pool = nn.MaxPool3d(kernel_size=(1, 2, 2))
+        self.pool = nn.MaxPool3d(kernel_size=(1, 3, 3))
+        #self.layer2 = nn.Conv3d(4, 4, stride=(1, 2, 2), kernel_size=5)
+        #self.pool2 = nn.MaxPool3d(kernel_size=(1, 2, 2))
         self.flatten = nn.Flatten()
-        self.layer2 = nn.Linear(7200, 24 * 64 * 64)
-        self.layer3 = nn.Linear(1152, 24 * 64 * 64)
+        self.layer3 = nn.Linear(3200, 24 * 64 * 64)
 
     def forward(self, x):
         x = x.unsqueeze(dim=1) / 1023.0
@@ -19,7 +20,6 @@ class Model(nn.Module):
         x = self.layer1(x)
         x = torch.relu(self.pool(x))
         x = self.flatten(x)
-        x = torch.relu(self.layer2(x))
         x = torch.relu(self.layer3(x))
 
         return x.view(-1, 24, 64, 64) * 1023.0
